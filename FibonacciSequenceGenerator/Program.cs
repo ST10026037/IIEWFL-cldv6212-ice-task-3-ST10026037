5e69f92356cee1a5dcb96bca63d67a7ec399da50
Omphile Hope Hlongwane <131029951+ST10026037@users.noreply.github.com>
2024-10-09 17:05:21 +0200

    Add files via upload

---
+++ FibonacciSequenceGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Storage.Queues;
+
+namespace FibonacciSequenceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = GenerateFibonacci(233);
+
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=hopehlongwanesa;AccountKey=F+VR0Ut91jSryb3DLGj7Kw8SCWCVjhLRA6TqtATqvWbtxtOLn48YmUM5efrPpqCtZNTgwue/tpIE+AStpkmt+Q==;EndpointSuffix=core.windows.net";
+            string queueName = "fibonacci-queue";
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+            {
+                foreach (var number in fibonacciNumbers)
+                {
+                    queueClient.SendMessage(number.ToString());
+                    Console.WriteLine($"Sent: {number}");
+                }
+            }
+        }
+
+        static List<int> GenerateFibonacci(int limit)
+        {
+            List<int> fibonacci = new List<int> { 0, 1 };
+            int nextNumber = 1;
+
+            while (nextNumber <= limit)
+            {
+                fibonacci.Add(nextNumber);
+                nextNumber = fibonacci[^1] + fibonacci[^2];
+            }
+
+            return fibonacci;
+        }
+    }
+}
